1286dcfb55447622c93e70a8c9bccadacdc0de9a
Abhay Verma <abhay.verma_cs23@gmail.com>
2024-03-10 10:25:49 +0530

    work file js

--- work.js
+++ work.js
@@ -47,3 +47,41 @@ function Rec(length,breadth){
     };
 }
 const place = new Rec(5,2);
+
+//dynamic nature of objects
+let circle2={
+    radius: 1
+}
+circle2.color="yellow"
+circle2.draw=function(){
+    console.log("draw")
+}
+delete circle2.color
+
+//org variable is not change (copy by value)-->(primuitive dt)
+let a1=100;
+let copy_a1=a1;
+
+//org obj is change with change in copy_obj (copy by reference)-->pointer(objects)
+let obj1={radius:10};
+let copy_obj1=obj1;
+
+//loop
+circle3={
+    radius:1,
+    color:"yellow",
+    draw: {x:1,y:20},
+    solid:0
+}
+for (let pi in circle3){
+    console.log(pi, circle3[pi]);
+}
+for(let i of Object.keys(circle3)){
+    console.log(i);
+}
+for(let i of Object.values(circle3)){
+    console.log(i);
+}
+for(let i of Object.entries(circle3)){
+    console.log(i);
+}
